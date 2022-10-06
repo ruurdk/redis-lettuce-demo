1fc791e38762b27900fe8aee3cc31e2506e2a5a7
Ruurd Keizer <ruurdk@hotmail.com>
2022-10-06 17:56:40 +0200

    exercise start

--- src/main/java/com/redis/lars/App.java
+++ src/main/java/com/redis/lars/App.java
@@ -19,54 +19,20 @@ public class App {
         RedisClient redisClient = RedisClient.create("redis://@localhost:6379");
         StatefulRedisConnection<String, String> connection = redisClient.connect();
 
-        // Set a key/string
-        connection.sync().set("hello", "World");
-
+        // Get and set a key/string
         System.out.println("Hello string: " + connection.sync().get("hello"));
 
-        // Set a key/hash
-        Map<String, String> myMap = new HashMap<>();
-        myMap.put("Name", "World");
-        myMap.put("Description", "Hello world");
-        myMap.put("Purpose", "42");
-        myMap.put("Thank", "You");
-        if (connection.sync().exists("lars:helloWorld") == 1L) {
-            connection.sync().del("lars:helloWorld");
-        }
-        connection.sync().hset("lars:helloWorld", myMap);
+        // Get and set a key/hash
         System.out.println("Hello hash: " + connection.sync().hgetall("lars:helloWorld"));
 
         RedisCommandFactory rcf = new RedisCommandFactory(connection);
-        TimeSeriesCommands tsc = rcf.getCommands(TimeSeriesCommands.class);
+        //TimeSeriesCommands tsc = rcf.getCommands(TimeSeriesCommands.class);
 
-        if (connection.sync().exists("myts") == 1L) {
-            connection.sync().del("myts");
-        }
+        // Set and get some timeseries data.
 
-        tsc.create("myts", 0);
-        tsc.add("myts", 1.0);
-        Thread.sleep(100);
-        tsc.add("myts", 1.3);
-        Thread.sleep(100);
-        tsc.add("myts", 1.5);
-        Thread.sleep(100);
-        tsc.add("myts", 1.2);
-        Thread.sleep(100);
-        tsc.add("myts", 1.4);
-
-        System.out.println("Hello TimeSeries: "
-                + tsc.range("myts", System.currentTimeMillis() - 1000 * 60 * 60 * 24 * 7, System.currentTimeMillis()));
+        //System.out.println("Hello TimeSeries: " + tsc.range("myts", System.currentTimeMillis() - 1000 * 60 * 60 * 24 * 7, System.currentTimeMillis()));
 
         RediSearchCommands rsc = rcf.getCommands(RediSearchCommands.class);
-
-        try {
-            rsc.drop("hello_idx");
-        } catch (RedisCommandExecutionException e) {
-            System.out.println("Error deleting index: " + e.getMessage());
-        }
         
-        rsc.create("hello_idx", "lars:", "Description");
-        System.out.println("Hello Search: " + rsc.searchWithHighlight("hello_idx", "wor*"));
-
     }
 }
--- src/main/java/com/redis/lars/TimeSeriesCommands.java
+++ src/main/java/com/redis/lars/TimeSeriesCommands.java
@@ -10,10 +10,8 @@ public interface TimeSeriesCommands extends Commands {
     @Command("TS.CREATE :key RETENTION :retentionTime ")
     void create(String key, long retentionTime);
 
-    @Command("TS.ADD :key * :value")
     void add(String key, double value);
 
-    @Command("TS.RANGE :key :from :to")
     Map<String, String> range(String key, long from, long to);
 
 }
